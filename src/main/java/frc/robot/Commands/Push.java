471b3194dac590df811f9907ed38e75f72262551
RailyaFarakhova <railya.farakhova@ht-la.org>
2024-02-15 17:40:51 -0800

    made pusher subsystem and it's command

---
+++ src/main/java/frc/robot/Commands/Push.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.Pusher;
+
+public class Push extends Command {
+
+  public Pusher m_pusher = new Pusher();
+
+  public Push(Pusher pusher) {
+    m_pusher = pusher; 
+  }
+
+ 
+  @Override
+  public void initialize() {}
+
+  
+  @Override
+  public void execute() {
+    m_pusher.controllerOn(.5);
+
+  }
+
+
+  @Override
+  public void end(boolean interrupted) {
+    m_pusher.controllerOff();
+
+  }
+
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/Subsystems/Pusher.java
+++ src/main/java/frc/robot/Subsystems/Pusher.java
@@ -2,21 +2,31 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-//package frc.robot.Subsystems;
+package frc.robot.Subsystems;
 
-//import edu.wpi.first.wpilibj.motorcontrol.Victor;
-//import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
 
-//public class Pusher extends SubsystemBase {
-  //TODO: we need to ask if we're doing a pusher at all
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.PusherConstants;
 
- // private Victor motorController;
+public class Pusher extends SubsystemBase {
 
-  //public Pusher() {
- // }
+  private Victor m_motorController = new Victor(PusherConstants.m_controllerID);
 
-  //@Override
-  //public void periodic() {
-    // This method will be called once per scheduler run
- // }
-//}
+  public Pusher() {
+    controllerOff();
+  }
+
+  public void controllerOn(double speed){
+    m_motorController.set(.5);
+  };
+
+  public void controllerOff(){
+    m_motorController.set(0);
+  };
+
+  @Override
+  public void periodic() {
+    
+  }
+}
