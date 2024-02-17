91773bf8bfb6bb0247b22c5ea40d10cbf4732c4b
RailyaFarakhova <railya.farakhova@ht-la.org>
2024-02-17 14:30:11 -0800

    added Limelight

---
+++ src/main/java/frc/robot/Constants/VisionConstants.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Constants;
+
+public class VisionConstants {
+    //TODO: tune these later
+    public static final double kHorizontal = 0;
+    public static final double kVertical = 0;
+    public static final double kVPW = 0 * Math.tan(Math.toRadians(kHorizontal / 0));
+    public static final double kVPH = 0 * Math.tan(Math.toRadians(kVertical / 0));
+    public static final double kImagecaptureLatency = 0 / 1000.0; 
+
+    public static final double kFloorToLens = 0;
+    public static final double kFloorToLensAngle = 0;
+    public static final double kFloorToTarget = 0 + (30/4);
+    public static final double kTargetHeight = 0;
+
+    public static final double kCameraFrameRate = 0;
+    public static final double kMinStability = 0;
+
+}
---
+++ src/main/java/frc/robot/Subsystems/Limelight.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.VisionConstants;
+
+public class Limelight extends SubsystemBase {
+
+  NetworkTable limelightNT = NetworkTableInstance.getDefault().getTable("Limelight");
+
+  double getAngle;
+  double getDistance;
+ 
+  public Limelight() {}
+
+  @Override
+  public void periodic() {
+    double getX = limelightNT.getEntry("x").getDouble(0);
+    double getY = limelightNT.getEntry("y").getDouble(0);
+
+    getDistance = (VisionConstants.kTargetHeight - VisionConstants.kFloorToLens) /
+      Math.tan(Math.toRadians(VisionConstants.kFloorToLensAngle + getY));
+
+    getAngle = getX;
+   
+  }
+}
