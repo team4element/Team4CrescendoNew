75996260f369caecef33f297a361559ee42fba05
Adam Garcia <adamgarcia@Adams-MacBook-Pro.local>
2024-02-24 21:38:43 -0500

    Shooter upgrade

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -5,6 +5,7 @@
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
@@ -14,75 +15,57 @@ import frc.robot.Constants.ShooterConstants;
 //import edu.wpi.first.math.ArmFeedForward;
 
 public class Shooter extends SubsystemBase {
+  private static TalonFX mLeader = new TalonFX(ShooterConstants.m_leftMotorID);
+  private static TalonFX m_follower = new TalonFX(ShooterConstants.m_rightMotorID);
 
-
-  private static TalonFX leftMotor = new TalonFX(ShooterConstants.m_leftMotorID);
-  private static TalonFX rightMotor = new TalonFX(ShooterConstants.m_rightMotorID);
-
-  Slot0Configs configsAmp = new Slot0Configs();
   Slot0Configs configsSpeaker = new Slot0Configs();
+  LiveDoubleBinding pBinding;
+  LiveDoubleBinding iBinding;
+  LiveDoubleBinding dBinding;
+  LiveDoubleBinding fBinding;
 
-  //Power
-  public static final double m_kFAmp = 2.0;
-  public static final double m_kFSpeaker = 25.0;
-  
-  LiveDoubleBinding pBinding = new LiveDoubleBinding("Shooter", "P", 0.5, (event) -> {
-    configsAmp.withKP(event.valueData.value.getDouble());
-  });
-
-  LiveDoubleBinding iBinding = new LiveDoubleBinding("Shooter", "I", 0.0, (event) -> {
-    configsAmp.withKI(event.valueData.value.getDouble());
-  });
-
-  LiveDoubleBinding dBinding = new LiveDoubleBinding("Shooter", "D", 0.25, (event) -> {
-    configsAmp.withKD(event.valueData.value.getDouble());
-  });
+  // Power
+  public double m_kFAmp = 2.0;
+  public double m_kFSpeaker = 25.0;
 
   final VelocityVoltage m_request = new VelocityVoltage(0).withSlot(0);
 
   public Shooter() {
-    configsAmp.kP = 0.5;
-    configsAmp.kI = 0.0;
-    configsAmp.kD = 0.25;
-    configsAmp.kV = 0.1;
-    configsAmp.kS = 0.0;
-    configsAmp.kA = 0.0;
-    configsAmp.kG = 0.0;
-    //10 percent
+    Slot0Configs leftMotorConfig = new Slot0Configs()
+        .withKS(0.0)
+        .withKV(0.1)
+        .withKP(0.5)
+        .withKI(0.0)
+        .withKD(0.25);
 
-    configsSpeaker.kP = 3;
-    configsSpeaker.kI = 0.0;
-    configsSpeaker.kD = 0.5;
-    configsSpeaker.kV = 0.1;
-    configsSpeaker.kS = 0.0;
-    configsSpeaker.kA = 0.0;
-    configsSpeaker.kG = 0.0;
+    pBinding = new LiveDoubleBinding("Shooter", "P", 0.5, (event) -> {
+      leftMotorConfig.withKP(event.valueData.value.getDouble());
+    });
 
-    motorsOff();
+    iBinding = new LiveDoubleBinding("Shooter", "I", 0.0, (event) -> {
+      leftMotorConfig.withKI(event.valueData.value.getDouble());
+    });
 
+    dBinding = new LiveDoubleBinding("Shooter", "D", 0.25, (event) -> {
+      leftMotorConfig.withKD(event.valueData.value.getDouble());
+    });
+
+    fBinding = new LiveDoubleBinding("Shooter", "F", 0.1, (event) -> {
+      m_kFSpeaker = event.valueData.value.getDouble();
+    });
+
+    mLeader.getConfigurator().apply(leftMotorConfig);
+    m_follower.getConfigurator().apply(leftMotorConfig);
+
+    m_follower.setControl(new Follower(mLeader.getDeviceID(), false));
   }
 
-  @Override
-  public void periodic() {
- }
-
- public void motorsOn(double setpoint) {
-    leftMotor.setControl(m_request.withVelocity(setpoint).withFeedForward(-m_kFSpeaker));
-    rightMotor.setControl(m_request.withVelocity(setpoint).withFeedForward(-m_kFSpeaker));
+  public void motorsOn(double setpoint) {
+    mLeader.setControl(m_request.withVelocity(setpoint).withFeedForward(-m_kFSpeaker));
 
     System.out.print("Left Error:");
-    System.out.print(leftMotor.getClosedLoopError());
+    System.out.print(mLeader.getClosedLoopError());
     System.out.print("| Right Error:");
-    System.out.println(rightMotor.getClosedLoopError());
-
-   // leftMotor.setVoltage(pid.calculate(encoder.getDistance(), setpoint) + feedforward);
-
- }
-
-
-  public void motorsOff() {
-    leftMotor.set(0);
-   rightMotor.set(0);
-
+    System.out.println(m_follower.getClosedLoopError());
   }
 }
