d25d874e97fa5dd9e03b6a7dd149f14abc40157e
Brandon Comins <BrandComin@gmail.com>
2024-03-06 20:39:19 -0800

    initialize limelight network table

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,9 @@
 
 package frc.robot;
 
-//import java.util.HashMap;
-//import java.util.Map;
+import edu.wpi.first.net.PortForwarder;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -20,6 +17,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    for (int port = 5800; port <= 5807; port++) 
+      PortForwarder.add(port, "limelight.local", port);
+    
     m_robotContainer = new RobotContainer();
   }
 
@@ -45,7 +45,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
-
   }
 
   @Override
