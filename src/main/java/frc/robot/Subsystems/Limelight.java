084f0dcd42a0e16554813069822b18ae424db9a7
Brandon Comins <BrandComin@gmail.com>
2024-03-06 20:39:56 -0800

    add functions to get angle and distance

--- src/main/java/frc/robot/Subsystems/Limelight.java
+++ src/main/java/frc/robot/Subsystems/Limelight.java
@@ -4,32 +4,67 @@
 package frc.robot.Subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants.VisionConstants;
+import frc.robot.Constants.VisionConstants;
 
 public class Limelight extends SubsystemBase {
 
-  NetworkTable limelightNT = NetworkTableInstance.getDefault().getTable("Limelight");
-//limelight helper ctre examples - swerve with pathplanner
+  private NetworkTable m_limelightNT;
+  private NetworkTableEntry m_tx; 
+  private NetworkTableEntry m_ty;
+  private NetworkTableEntry m_ta;
 
-//drivve apply request supplier robot centric, withRotationaRate(vision,getaprilTagTX())
-//vision has commands returns limelight values
-//auto-aline to button
-  double getAngle;
-  double getDistance;
+  private double m_x;
+  private double m_y;
+  private double m_area;
  
-  public Limelight() {}
+  public Limelight() {
+    m_limelightNT = NetworkTableInstance.getDefault().getTable("limelight");
+    m_tx = m_limelightNT.getEntry("tx");
+    m_ty = m_limelightNT.getEntry("ty");
+    m_ta = m_limelightNT.getEntry("ta");
+  }
+
+
+  /**
+   * Gets the distance of the target from the Limelight in inches
+   * @return Distance in inches 
+   */
+  public double getDistance()
+  {
+    return (VisionConstants.goalHeightInches - VisionConstants.limelightLensHeightInches) / Math.tan(getAngleToGoalRadians());
+  }
+
+  /**
+   * Gets the angle of the of the target from the Limelight
+   * @return Angle in degrees
+   */
+  public double getAngleToTargetDegrees()
+  {
+    return VisionConstants.limelightMountAngleDegrees + m_y;
+  }
+
+  /**
+   * Gets the angle to the goal in radians
+   * @return Angle in radians
+   */
+  public double getAngleToGoalRadians()
+  {
+    return getAngleToTargetDegrees() * (Math.PI/180.0);
+  }
 
   @Override
   public void periodic() {
-    // double getX = limelightNT.getEntry("x").getDouble(0);
-    // double getY = limelightNT.getEntry("y").getDouble(0);
+    m_x    = m_tx.getDouble(0.0);
+    m_y    = m_ty.getDouble(0.0);
+    m_area = m_ta.getDouble(0.0);
 
-  //   getDistance = (VisionConstants.kTargetHeight - VisionConstants.kFloorToLens) /
-  //     Math.tan(Math.toRadians(VisionConstants.kFloorToLensAngle + getY));
+    SmartDashboard.putNumber("LimeLightX"   , m_x);
+    SmartDashboard.putNumber("LimeLightY"   , m_y);
+    SmartDashboard.putNumber("LimeLightArea", m_area);
 
-  //   getAngle = getX;
-   
   }
 }
