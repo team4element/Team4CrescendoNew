2f0cc0c9baa291fd53a5a00076c57383e69bf725
Brandon Comins <BrandComin@gmail.com>
2024-03-16 16:35:23 -0700

    change CanCoder to pheonix 5

--- src/main/java/frc/robot/Subsystems/Pusher.java
+++ src/main/java/frc/robot/Subsystems/Pusher.java
@@ -6,25 +6,27 @@ package frc.robot.Subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.sensors.CANCoder;
 import frc.robot.Constants.PusherConstants;
 
 public class Pusher extends SubsystemBase {
-  
+
   public enum Direction {
     SHOOT, RESET
   }
-  
+
   private VictorSPX m_motorController;
-  private CANcoder m_encoder;
+  private CANCoder m_encoder;
 
 
   public Pusher() {
     m_motorController = new VictorSPX(PusherConstants.motorId);
-    m_encoder = new CANcoder(PusherConstants.encoderID);
+    m_encoder = new CANCoder(PusherConstants.encoderID);
 
     m_motorController.configFactoryDefault();
+
+    m_motorController.configRemoteFeedbackFilter(m_encoder, 0);
     m_motorController.config_kP(0, .3);
     m_motorController.config_kI(0, 0.);
     m_motorController.config_kD(0, 0);
@@ -38,7 +40,7 @@ public class Pusher extends SubsystemBase {
   public void controllerOff(){
     m_motorController.set(ControlMode.PercentOutput, 0);
   };
-  
+
   public void setToPosition(double position){
     m_motorController.set(ControlMode.Position, position);
   };
@@ -49,7 +51,7 @@ public class Pusher extends SubsystemBase {
 
   public double getEncoderPostion()
   {
-    return m_encoder.getAbsolutePosition().getValue() - PusherConstants.encoderOffset;
+    return m_encoder.getAbsolutePosition();
   }
 
   @Override
