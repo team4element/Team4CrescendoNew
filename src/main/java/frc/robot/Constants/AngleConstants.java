c366d5eafd8c90cd5b2c950c0da35f08a8b6145f
RailyaFarakhova <railya.farakhova@ht-la.org>
2024-03-16 10:44:11 -0700

    made a base class for the variable angle

---
+++ src/main/java/frc/robot/Constants/AngleConstants.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Constants;
+
+/** Add your docs here. */
+public class AngleConstants {
+//TODO: change ids later
+    public static final int m_followerID = 0;
+    public static final int m_leaderID = 0;
+
+    public static final int Ks = 0;
+    public static final int Kg = 0;
+    public static final int Kv = 0;
+    public static final int Ka = 0;
+}
