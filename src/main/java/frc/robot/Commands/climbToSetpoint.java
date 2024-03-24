d9063d9940ed3e9cc746a2c663b82bb052115dbb
RailyaFarakhova <railya.farakhova@ht-la.org>
2024-03-24 16:21:39 -0700

    added file to help bring climb to setpoint

---
+++ src/main/java/frc/robot/Commands/climbToSetpoint.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.Climber;
+
+public class climbToSetpoint extends Command {
+
+  Climber m_climber = new Climber();
+
+  public climbToSetpoint(Climber climber) {
+ 
+    m_climber = climber;
+  
+  }
+
+
+  @Override
+  public void initialize() {}
+
+
+  @Override
+  public void execute() {
+    m_climber.goToSetPoint();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_climber.motorsOff();
+    m_climber.brake();
+  }
+
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
